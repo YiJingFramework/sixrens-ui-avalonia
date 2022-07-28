21ba786918e2d1cc3de4f7a564a4c26267746fbb
yueyinqiu <lujiaqi2003@126.com>
2022-07-28 20:47:30 +0800

    with nuget

---
+++ SixRens.UI.Avalonia/SixRens.UI.Avalonia/App.axaml.cs
@@ -0,0 +1,28 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using SixRens.UI.Avalonia.ViewModels;
+using SixRens.UI.Avalonia.Views;
+
+namespace SixRens.UI.Avalonia
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ SixRens.UI.Avalonia/SixRens.UI.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SixRens.UI.Avalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
