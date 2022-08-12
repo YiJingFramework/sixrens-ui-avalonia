11077dac6fef881efc8229934bc4078b1b4d4f5c
yueyinqiu <lujiaqi2003@126.com>
2022-08-12 11:47:48 +0800

    up sth

--- SixRens.UI.Avalonia/SixRens.UI.Avalonia/Views/MainWindow.axaml.cs
+++ SixRens.UI.Avalonia/SixRens.UI.Avalonia/Views/MainWindow.axaml.cs
@@ -1,13 +1,17 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using SixRens.Core.占例存取;
 using SixRens.UI.Avalonia.Services.SixRens;
 using System;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SixRens.UI.Avalonia.Views
 {
     public partial class MainWindow : Window
     {
-        private SixRensCore sixRensCore;
+        private readonly SixRensCore sixRensCore;
 
         public MainWindow()
         {
@@ -15,6 +19,25 @@ namespace SixRens.UI.Avalonia.Views
 
             var sixrensSavingsPath = Path.GetFullPath("savings", AppContext.BaseDirectory);
             sixRensCore = new SixRensCore(new(sixrensSavingsPath));
+        }
+        private async void OpenDivinationCaseAsync(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog() {
+                AllowMultiple = false,
+                Filters = new() {
+                    new() {
+                        Extensions = new() {
+                            "srs"
+                        },
+                        Name = "占例文件"
+                    }
+                }
+            };
+            var r = await dialog.ShowAsync(this);
+            if (r is null)
+                return;
+            var s = r.Single();
+            var c = 占例.反序列化(s);
         }
     }
 }
